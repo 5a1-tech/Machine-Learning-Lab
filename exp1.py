86bdd6b3ff39698cec0febff6fd6ea2218c3b122
5a1-tech <69663997+5a1-tech@users.noreply.github.com>
2022-04-06 14:48:22 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "15.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "p_A_B=0.03\n",
+    "p_f=0.2\n",
+    "p_a__f=0.03/0.2\n",
+    "res = (p_a__f)*100;\n",
+    "print(res)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
